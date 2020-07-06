0866d9eea4701b8290eeddfe6d5973114186912b
galih asmarandaru <galih.asmarandaru@gmail.com>
2020-07-06 18:37:51 +0700

    add 360 view

---
+++ data.js
@@ -0,0 +1,120 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-spherical",
+      "name": "spherical",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -3.0728267834853007,
+        "pitch": 0.3075653799686169,
+        "fov": 1.4311086604234626
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9362308883501456,
+          "pitch": 0.27368351179280914,
+          "rotation": 18.84955592153877,
+          "target": "1-picture1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0977149141889493,
+          "pitch": -0.0586349564611055,
+          "title": "Bar",
+          "text": "Tempat anda membeli makanan dan kue"
+        }
+      ]
+    },
+    {
+      "id": "1-picture1",
+      "name": "picture1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5640161977417364,
+          "pitch": 0.18979191806415585,
+          "rotation": 0,
+          "target": "2-picture2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2597761545746646,
+          "pitch": -0.1857892921540767,
+          "title": "Kebersamaan",
+          "text": "Dengan teman anda dunia terasa bahagia"
+        }
+      ]
+    },
+    {
+      "id": "2-picture2",
+      "name": "picture2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
